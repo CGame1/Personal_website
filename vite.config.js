cec7eb1399d4915f85962314c51747d1214f89f1
Chloe Game <chloe.game@uib.no>
2026-02-12 11:44:37 +0100

    updated google scholar info

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Using relative base for flexibility
+  base: '/Personal_website/', // Base path for GitHub Pages
 })
